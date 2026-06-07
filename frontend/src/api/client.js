18b4ef75052cfdde6ee530a7b3ced20ef68e28ad
User <chanhi421@naver.com>
2026-06-07 23:38:03 +0900

    deploy: 배포용 url로 변경

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'http://localhost'; // 백엔드 주소
+const API_BASE_URL = "/api/v1";
 
 const client = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
